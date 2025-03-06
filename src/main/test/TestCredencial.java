8c4a46ec6634af376c7bf6bbd4a9fec892bf35e5
= <=>
2025-03-06 09:30:16 +0100

    Añadimos un nuevo paquete que contendrar las pruebas de JUnit5

---
+++ src/main/test/TestCredencial.java
@@ -0,0 +1,34 @@
+package main.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCredencial {
+
+	@Test
+	void testCredencial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testComprobarPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEsPasswordSegura() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/main/test/TestUsuario.java
@@ -0,0 +1,39 @@
+package main.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestUsuario {
+
+	@Test
+	void testUsuarioStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUsuarioStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEsCuentaBloqueada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testModificarPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEsPasswordSegura() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHacerLogin() {
+		fail("Not yet implemented");
+	}
+
+}
